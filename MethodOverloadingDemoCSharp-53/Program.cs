60bfa5d64e48a14203b8a203743637fcb1c4eef8
Seth Bowman <brandonsethbowman@gmail.com>
2024-12-17 14:24:53 -0600

    demo completed

---
+++ MethodOverloadingDemoCSharp-53/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace MethodOverloadingDemoCSharp_53;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello, World!");
+        // Console.WriteLine(true);
+        // Console.WriteLine(23);
+        // Console.WriteLine('d');
+
+        var addedNumbers = Add(2.5, 2.5);
+        Console.WriteLine(addedNumbers);
+        
+        Greet("Seth", "Bowman", 30);
+        Greet();
+    }
+
+    public static int Add(int numOne, int numTwo)
+    {
+        return numOne + numTwo;
+    }
+
+    public static double Add(double numOne, double numTwo)
+    {
+        return numOne + numTwo;
+    }
+
+    public static void Greet(string firstName)
+    {
+        Console.WriteLine($"Hello, {firstName}!");
+    }
+
+    public static void Greet(string firstName, string lastName)
+    {
+        Console.WriteLine($"Hello, {firstName} {lastName}!");
+    }
+
+    public static void Greet(string firstName, string lastName, int age)
+    {
+        if (age >= 21)
+        {
+            Console.WriteLine($"Hello, {firstName} {lastName}! You are allowed in to the bar.");
+        }
+        else
+        {
+            Console.WriteLine($"Hello, {firstName} {lastName}! Sorry, you are not allowed in to the bar.");
+        }
+    }
+
+    public static void Greet()
+    {
+        Console.WriteLine("Hello!");
+    }
+}
